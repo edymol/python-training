d8aa98cbd33a03fe7256bc7399173acf134225f7
edymol <edymolinap@gmail.com>
2024-04-07 15:19:24 +0100

    add: remove vertex method

--- algorithms/Algo_implementation/graphs.py
+++ algorithms/Algo_implementation/graphs.py
@@ -5,6 +5,7 @@ class Graph:
     def print_graph(self):
         for vertex in self.adj_list:
             print(vertex, ':', self.adj_list[vertex])
+
     def add_vertex(self, vertex):
         if vertex not in self.adj_list.keys():
             self.adj_list[vertex] = []
@@ -18,19 +19,78 @@ class Graph:
             return True
         return False
 
-# TEST ADD EDGE
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+# TEST REMOVE VERTEX
 my_graph = Graph()
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
 
-my_graph.add_vertex(1)
-my_graph.add_vertex(2)
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('A', 'C')
+my_graph.add_edge('A', 'D')
+my_graph.add_edge('B', 'D')
+my_graph.add_edge('C', 'D')
 
-my_graph.add_edge(1,2)
-
+print('Graph before remove_vertex():')
 my_graph.print_graph()
+
+my_graph.remove_vertex('D')
+
+print('\nGraph after remove_vertex():')
+my_graph.print_graph()
+
+# TEST REMOVE EDGE
+# my_graph = Graph()
+# my_graph.add_vertex('A')
+# my_graph.add_vertex('B')
+# my_graph.add_vertex('C')
+#
+# my_graph.add_edge('A','B')
+# my_graph.add_edge('B','C')
+# my_graph.add_edge('C','A')
+#
+# print('Graph before remove_edge():')
+# my_graph.print_graph()
+#
+#
+# my_graph.remove_edge('A','C')
+#
+#
+# print('\nGraph after remove_edge():')
+# my_graph.print_graph()
+
+# TEST ADD EDGE
+# my_graph = Graph()
+#
+# my_graph.add_vertex(1)
+# my_graph.add_vertex(2)
+#
+# my_graph.add_edge(1,2)
+#
+# my_graph.print_graph()
 
 # TEST ADD VERTEX
-my_graph = Graph()
+# my_graph = Graph()
+# my_graph.add_vertex('A')
 
-my_graph.add_vertex('A')
-
-my_graph.print_graph()
+# my_graph.print_graph()
