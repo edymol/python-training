6c02f8bba1c57e7b03005000e1e769e35bcb2312
edymol <edymolinap@gmail.com>
2024-04-07 12:22:32 +0100

    add: get item method

--- algorithms/Algo_implementation/hash_tables.py
+++ algorithms/Algo_implementation/hash_tables.py
@@ -22,15 +22,33 @@ class HashTable:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
 
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
 
-# TEST SET ITEM
+
+# TEST GET ITEM
 my_hash_table = HashTable()
 
 my_hash_table.set_item('bolts', 1400)
 my_hash_table.set_item('washers', 50)
-my_hash_table.set_item('lumber', 70)
 
-my_hash_table.print_table()
+print('Bolts:', my_hash_table.get_item('bolts'))
+print('Washers:', my_hash_table.get_item('washers'))
+print('Lumber:', my_hash_table.get_item('lumber'))
+
+# TEST SET ITEM
+# my_hash_table = HashTable()
+#
+# my_hash_table.set_item('bolts', 1400)
+# my_hash_table.set_item('washers', 50)
+# my_hash_table.set_item('lumber', 70)
+#
+# my_hash_table.print_table()
 
 # TEST HASH TABLE CONSTRUCTOR
 # my_hash_table = HashTable()
